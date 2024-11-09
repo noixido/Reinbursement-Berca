9db94d9faa17421558e2146c8730b11cdb75ad9b
noixido <noixido@gmail.com>
2024-11-09 19:12:30 +0700

    menambanhkan CRUD Account

--- API/Models/Category.cs
+++ API/Models/Category.cs
@@ -6,9 +6,9 @@ namespace API.Models
     public class Category
     {
         [Key]
-        public string? Id_Category { get; set; }
+        public string Id_Category { get; set; }
         public string Category_Name { get; set; }
 
-        public ICollection<Reimbursement>? Reimbursements { get; set; }
+        public ICollection<Reimbursement> Reimbursements { get; set; }
     }
 }
--- API/Models/Title.cs
+++ API/Models/Title.cs
@@ -6,10 +6,10 @@ namespace API.Models
     public class Title
     {
         [Key]
-        public string? Id_Title { get; set; }
+        public string Id_Title { get; set; }
         public string Title_Name { get; set; }
-        public float? Reimburse_Limit { get; set; }
+        public int Reimburse_Limit { get; set; }
 
-        public ICollection<AccountDetail>? AccountDetails { get; set; }
+        public ICollection<AccountDetail> AccountDetails { get; set; }
     }
 }
