e24ee37b5fa98112cbb9cc223a1e7bc3f9872f13
Edo Laksana <edolaksana02@gmail.com>
2024-11-11 14:24:07 +0700

    Handling

--- API/Models/Title.cs
+++ API/Models/Title.cs
@@ -11,10 +11,7 @@ namespace API.Models
         public string Title_Name { get; set; }
         public float Reimburse_Limit { get; set; }
 
-<<<<<<< HEAD
-=======
         [JsonIgnore]
->>>>>>> 44d286580ab68d5dcf022f2c1abca43f923cc16f
         public ICollection<AccountDetail>? AccountDetails { get; set; }
     }
 }
