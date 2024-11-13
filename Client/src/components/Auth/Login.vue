3db07480170ff2ef5b8ca3a174e1a24475f8be96
noixido <noixido@gmail.com>
2024-11-13 16:41:26 +0700

    mempercantik tampilan login

--- Client/src/components/Auth/Login.vue
+++ Client/src/components/Auth/Login.vue
@@ -1,17 +1,21 @@
 <template>
-    <div>
-        <div class="card bg-white text-primary-content w-96 mx-auto mt-72">
-            <div class="card-body">
-                <h2 class="card-title">Login</h2>
-                <form id="login">
-                    <input type="email" v-model="email" class="bg-white border rounded-md p-3 w-full mb-2" name="email" id="email" placeholder="Email">
-                    <input :type="showPassword ? 'text' : 'password'" v-model="password" class="bg-white border rounded-md p-3 w-full mb-2" name="password" id="password" placeholder="Password">
+    <div class="bg-grey-100 min-h-screen flex items-center justify-center">
+        <div class="card bg-white text-primary-content w-96 mx-auto border-red-500 border-2">
+            <div class="card-body ">
+                <!-- <h2 class="card-title">Login</h2> -->
+                <div class="w-48 mx-auto text-center flex flex-col gap-2">
+                    <img src="../../assets/images/logo.png" alt="Becare logo">
+                    <span class="font-bold text-red-600 text-lg w-full">Berca Reimbursement</span>
+                </div>
+                <form id="login" class="mt-5">
+                    <input type="email" v-model="email" class="bg-white rounded-md p-3 w-full mb-2 border-red-500 border" name="email" id="email" placeholder="Email">
+                    <input :type="showPassword ? 'text' : 'password'" v-model="password" class="bg-white rounded-md p-3 w-full mb-2 border-red-500 border" name="password" id="password" placeholder="Password">
                     <label for="showPass">
                         <input type="checkbox" v-model="showPassword" id="showPass"> Show Password
                     </label>
                 </form>
                 <div class="card-actions justify-end">
-                    <button type="submit" form="login" class="btn bg-primary text-white border-none" @click.prevent="login">
+                    <button type="submit" form="login" class="btn text-white border-none w-full mt-5 bg-red-600 hover:bg-red-500 " @click.prevent="login">
                         <div :class="stateLoading ? 'loading loading-spinner loading-sm text-white' : ''">
                             Login
                         </div>
